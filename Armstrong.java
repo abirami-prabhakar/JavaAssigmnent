6908b9bfa0eff396e00ee9627cfc39e5f4539c9d
Abirami Prabhakar <abiramiprabhakar0831@gmail.com>
2026-04-15 00:29:12 +0530

    Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -8,7 +8,7 @@ public class Armstrong {
         int t = n, s = 0, d = (""+n).length();
         while (t > 0) {
             int r = t % 10, p = 1;
-            for (int i = 0; i < d; i++) p *= r; // manual power
+            for (int i = 0; i < d; i++) p *= r; 
             s += p;
             t /= 10;
         }
